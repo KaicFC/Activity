3a9d9c3e459ce1d1e53fa6858590eb2eb377a5b4
Kaic Carvalho <kaic15_fernandes@hotmail.com>
2021-01-09 14:36:36 -0300

    Criação da segunda activity e recuperação de dados entre as activities.

--- app/src/main/java/com/cursoandroid/activity/MainActivity.java
+++ app/src/main/java/com/cursoandroid/activity/MainActivity.java
@@ -2,13 +2,40 @@ package com.cursoandroid.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Definir intent a ser passado no startActivity e qual será a Acitivity.
+                Intent intent = new Intent(getApplicationContext(), SegundaActivity.class);
+
+                //Passar Dados
+                intent.putExtra("nome", "Kaic");
+                intent.putExtra("idade", 23);
+
+                //Abrir a Activity
+                startActivity(intent);
+
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/cursoandroid/activity/SegundaActivity.java
@@ -0,0 +1,35 @@
+package com.cursoandroid.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    private TextView textNome, textIdade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+
+        textNome = findViewById(R.id.textNome);
+        textIdade = findViewById(R.id.textIdade);
+
+        //Recuperar Dados de uma Activity para outra - BUNDLE
+        // getExtras recupera todos os dados(Array)
+        Bundle dados = getIntent().getExtras();
+        String nome = dados.getString("nome");
+        int idade = dados.getInt("idade");
+
+        //Configurar valores recuperados
+        textNome.setText(nome);
+        textIdade.setText(String.valueOf(idade));
+
+
+    }
+
+
+
+}
